37ec5731e9edd5720dde255a6ca312e7c0f68398
ExQuid <ExQuid@users.noreply.github.com>
2017-10-12 21:58:53 +0200

    Add files via upload

---
+++ MathProject-Collatz_Branch_Patch_1/yeye2/yeye2/mather.cpp
@@ -0,0 +1,61 @@
+#pragma once
+#include "stdafx.h"
+#include "mather.h"
+#include <iostream>"
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int counter = 0;
+vector<string> collection = { "collatz", "help" };
+
+mather::mather() {
+
+}
+
+ int mather::mainCalc(int a) {
+	while (a > 1) {
+
+		if (a % 2 == 0) {
+			a = a / 2;
+			counter = counter + 1;
+		}
+		else {
+			a = a * 3 + 1;
+			counter = counter + 1;
+		}
+		
+	}
+
+	return a;
+}
+
+string mather::input(string a) {
+	transform(a.begin(), a.end(), a.begin(), ::tolower);
+	for (int i = 0; i < collection.size(); i++)
+	{
+		if (a == collection[0])
+		{
+			cout << "Please enter the number you want to calculate: ";
+			int numberToHold = 0;
+			cin >> numberToHold;
+			mainCalc(numberToHold);
+			cout << "It took: " << counter << " steps to calculate the number." << endl;
+
+			counter = 0;
+
+		}
+		else if (a == collection[1])
+		{
+			cout << "Available commands: " << endl;
+			for (int i = 0; i < collection.size(); i++)
+			{
+				cout << collection[i] << endl;
+			}
+
+		}
+		
+		return a;
+	}
+}
